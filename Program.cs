e32b6e96bc6a277087e691ff6265b85f25321245
Caio Henrique <caioadriana5@gmail.com>
2021-06-02 11:22:28 -0300

    malditoran

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ namespace listaobjs
         {
             bool rep1 = false;
             bool rep2 = false;
-            bool rep3 = false;
 
             List<Produto> produtos = new List<Produto>();
 
@@ -51,42 +50,66 @@ namespace listaobjs
                             Console.WriteLine($@"
                         Código do produto: {item.id}
                         Nome do produto: {item.Nome}
-                        Preço do produto: {item.Preco}
+                        Preço do produto: {item.Preco:C2}
                         Quantidade em estoque: {item.QuantiEstoque}
                         ");
 
-                            Console.WriteLine("Você será levado ao menu de novo.");
-                            rep1 = true;
                         }
+                        Console.WriteLine("Você será levado ao menu de novo.");
+                        rep1 = true;
                         break;
                     case 3:
                         Console.WriteLine("Qual o nome do produto que você deseja atualizar?");
-                        string nomeado = Console.ReadLine();
+                        string nomemud = Console.ReadLine();
 
+                        do
+                        {
 
-                        Console.WriteLine(@"
+                            Console.WriteLine(@"
                 __________________________________
                 |     BEM VINDO AO MENU MUDAR    |
                 | 1-Mudar nome do produto        |
                 | 2-Mudar Preço do produto       |
                 | 3-Mudar quantidade em estoque  |
                 ----------------------------------");
-                        int escolha = int.Parse(Console.ReadLine());
+                            int escolha = int.Parse(Console.ReadLine());
 
-                        switch (escolha)
-                        {
-                            case 1:
-                            Produto atualizar = produtos.Find(item => item.Nome == nomeado);
-                                break;
-                            case 2:
-                            Produto atualizar2 = produtos.Find(item => item.Nome == nomeado);
-                                break;
-                            case 3:
-                            Produto atualizar3 = produtos.Find(item => item.Nome == nomeado);
-                                break;
-                            default:
-                                break;
-                        }
+                            switch (escolha)
+                            {
+                                case 1:
+                                    Console.WriteLine("Qual o novo nome do produto?");
+                                    string novonome = Console.ReadLine();
+
+                                    produtos.Find(item => item.Nome == nomemud).Nome = novonome;
+
+                                    Console.WriteLine("Você será levado ao menu de novo.");
+                                    rep2 = false;
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Qual o novo preço do produto?");
+                                    float novopreco = float.Parse(Console.ReadLine());
+
+                                    produtos.Find(item => item.Nome == nomemud).Preco = novopreco;
+
+                                    Console.WriteLine("Você será levado ao menu de novo.");
+                                    rep2 = false;
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Qual o novo estoque do produto?");
+                                    int novoestoque = int.Parse(Console.ReadLine());
+
+                                    produtos.Find(item => item.Nome == nomemud).QuantiEstoque = novoestoque;
+
+                                    Console.WriteLine("Você será levado ao menu de novo.");
+                                    rep2 = false;
+                                    break;
+                                default:
+                                    Console.WriteLine("Numero invalido, lhe falta inteligencia, volte desde o começo e faça certo agora.");
+                                    rep2 = true;
+                                    break;
+                            }
+                        } while (rep2);
+                        rep1 = true;
                         break;
                     case 4:
                         Console.WriteLine("Qual o nome do produto que você deseja excluir?");
@@ -99,6 +122,7 @@ namespace listaobjs
                         rep1 = true;
                         break;
                     case 5:
+                        Console.WriteLine("Tchauzinho usuario.");
                         rep1 = false;
                         break;
                     default:
